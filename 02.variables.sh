707dd4b184708c412203012ada5c37aafe5da143
adityamitv <shushma.valmiki@gmail.com>
2024-04-21 15:26:04 +0530

    hello script

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,6 +1,6 @@
-#!/git/bin
+#!/bin/bash
 
-echo"Ramesh:: hello Suresh,how r u?"
-echo"Suresh:: Hi, Ramesh,i am fn. how r u?"
-echo"Ramesh:: I am fn too.how is ur work?"
-echo"Suresh:: not bad.i am Thinking to upgrade to DevOps"
+echo "Ramesh:: hello Suresh,how r u?"
+echo "Suresh:: Hi, Ramesh,i am fn. how r u?"
+echo "Ramesh:: I am fn too.how is ur work?"
+echo "Suresh:: not bad.i am Thinking to upgrade to DevOps"
