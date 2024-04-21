2425472d16c8dcf15e7063c5a67744eca64c61f6
adityamitv <shushma.valmiki@gmail.com>
2024-04-21 14:56:19 +0530

    hello script

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/git/bin
+
+person1=Ramesh #no space between = and value
+person2=Suresh
+
+echo"$person1:: hello $person2,how r u?"
+echo"$person2:: Hi $person1,i am fn. how r u?"
+echo"$person1:: I am fn too.how is ur work?"
+echo"$person2:: not bad.i am Thinking to upgrade to DevOps"
