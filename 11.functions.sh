f62b8ffcd737f55240989ff54bef73dd2356a61b
adityamitv <shushma.valmiki@gmail.com>
2024-04-21 22:32:49 +0530

    hello script

--- 11.functions.sh
+++ 11.functions.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 VALIDATE(){
     echo "Exit status: $1"
-    echo "What r u doing:$2"
+    echo "What are you doing: $2"
 }
 
 if [ $USERID -ne 0 ]
 then
-   echo "please ru this script with root access."
+   echo "please run this script with root access."
    exit 1 # manually exit if error comes.
 else
      echo "you are super user."
