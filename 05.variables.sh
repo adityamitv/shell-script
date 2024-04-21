79ea98ec87d76da7aa54a7c7dec6724eb44ee9e6
adityamitv <shushma.valmiki@gmail.com>
2024-04-21 15:54:02 +0530

    hello script

---
+++ 05.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Pleae enter username::"
+
+read USERNAME #here USERNAME is variable
+
+echo "Please enter password::"
+
+read PASSWORD
+
+echo "Username is: $USERNAME, PASSWORD is: $PASSWORD"
