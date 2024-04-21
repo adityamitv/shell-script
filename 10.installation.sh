93aba828a463ccdf1d8bbad33d0ea3c06245affd
adityamitv <shushma.valmiki@gmail.com>
2024-04-21 21:14:47 +0530

    hello script

--- 10.installation.sh
+++ 10.installation.sh
@@ -3,21 +3,32 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then 
-  echo "please run this script with root access."
-  exit 1 #manually exit if error comes.
+then
+   echo "please ru this script with root access."
+   exit 1 # manually exit if error comes.
 else
-   echo "you are super user."
+     echo "you are super user."
 fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
-    echo "Installation of mysql...Failure"
-    exit 1
-fi
+  echo "Installation of mysql...FAILURE"
+  exit 1
+else
+   echo "Installation of mysql...SUCCESS"
+fi 
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of git...SUCCESS"
+
+fi
 
 echo "is script proceeding?"
