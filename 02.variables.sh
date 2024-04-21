2a759458b5b74674386f986f9b267e1bed376368
adityamitv <shushma.valmiki@gmail.com>
2024-04-21 12:08:29 +0530

    adding variables

---
+++ 02.variables.sh
@@ -0,0 +1,6 @@
+#!/git/bin
+
+echo"Ramesh:: hello Suresh,how r u?"
+echo"Suresh:: Hi, Ramesh,i am fn. how r u?"
+echo"Ramesh:: I am fn too.how is ur work?"
+echo"Suresh:: not bad.i am Thinking to upgrade to DevOps"
