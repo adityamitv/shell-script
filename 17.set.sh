8466d09e5f8b24d5056fd46b9afe337382a086ee
adityamitv <shushma.valmiki@gmail.com>
2024-04-24 13:16:25 +0530

    hello type

---
+++ 17.set.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at $1: $2"
+}
+ trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+ 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+   echo "please ru this script with root access."
+   exit 1 # manually exit if error comes.
+else
+     echo "you are super user."
+fi
+
+dnf install mysql -y
+dnf install git -y
+
+echo "is script proceeding?"
